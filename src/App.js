ad21d42c98b5be4c5182667b322c77fb454588c2
Pedro <pkupersmit@gmail.com>
2020-08-01 12:55:12 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactFullpage from "@fullpage/react-fullpage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends React.Component {
+  onLeave(origin, destination, direction) {
+    console.log("Leaving section " + origin.index);
+  }
+  afterLoad(origin, destination, direction) {
+    console.log("After load: " + destination.index);
+  }
+
+  render() {
+    return (
+      <ReactFullpage
+        scrollOverflow={true}
+        onLeave={this.onLeave.bind(this)}
+        afterLoad={this.afterLoad.bind(this)}
+        render={({ state, fullpageApi }) => {
+          return (
+            <div id="fullpage-wrapper">
+              <div className="section">
+                <h1>Hola, me llamo Pedro ... texto a desarrollar</h1>
+                <FontAwesomeIcon icon={FaSass} />
+              </div>
+              <div className="section">
+                <div className="social-media">
+                  {" "}
+                  aca van los iconos social media
+                </div>
+              </div>
+              <div className="section">
+                <p>aca van los skills</p>
+              </div>
+              <div className="section">
+                <p>aca van los proyectos</p>
+              </div>
+              <div className="section">
+                <p>aca van un boton de contacto</p>
+              </div>
+            </div>
+          );
+        }}
+      />
+    );
+  }
 }
 
 export default App;
+
+// <div id="fullpage-wrapper">
+//   <div className="section">
+//
+//   </div>
+//   <div className="section">
+//
+//   </div>
+//
+//
+//   </div>
+// </div>
---
+++ src/components/Skills/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Skills extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>SKILLS</h2>
+        <div>Aca van los íconos de skills</div>
+      </div>
+    );
+  }
+}
+
+export default Skills;
